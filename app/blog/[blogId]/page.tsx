6f8d554b06c78d9a9b9612ce767267fa9eb6d5e6
LModipane <modipanesh@gmail.com>
2026-07-04 11:02:59 +0200

    feat: add searchParams to Home component and pass comment to CommentsSection

--- app/blog/[blogId]/page.tsx
+++ app/blog/[blogId]/page.tsx
@@ -1,14 +1,23 @@
 import { CommentsSection, MarkdownSection } from '@/components/index';
 
-export default async function Home({ params }: { params: Promise<{ blogId: string }> }) {
+export default async function Home({
+	params,
+	searchParams,
+}: {
+	params: Promise<{ blogId: string }>;
+	searchParams: Promise<{
+		comment?: string;
+	}>;
+}) {
 	const { blogId } = await params;
+	const { comment } = await searchParams;
 	return (
 		<main className="h-full w-full flex flex-col text-slate-800 bg-white overflow-x-hidden">
 			{/* Add Header section */}
 			{/* Body: */}
 			<MarkdownSection content={null} />
 			{/* Comments */}
-			<CommentsSection pageId={`blog:${blogId}`} />
+			<CommentsSection pageId={`blog:${blogId}`} formStatus={comment} />
 			{/* footer */}
 			<footer className="h-full text-white bg-black mt-auto p-2 flex items-center justify-center">
 				footer
