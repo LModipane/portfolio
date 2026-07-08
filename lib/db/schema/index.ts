4dc072017a28b0c62a592709bdec54e4fa1ed75e
LModipane <modipanesh@gmail.com>
2026-07-08 20:38:24 +0200

    feat: added end-user Profile when signed in

--- lib/db/schema/index.ts
+++ lib/db/schema/index.ts
@@ -1,7 +1,25 @@
+import { relations } from 'drizzle-orm';
 import { pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+
+export const profileTable = pgTable('profile', {
+	id: uuid('id').primaryKey().defaultRandom(),
+	imageUrl: varchar('image_url'),
+	name: varchar('name').notNull(),
+	email: varchar('email').notNull(),
+	userId: varchar('user_id').notNull(),
+	createdAt: timestamp('create_at', { mode: 'date' }).defaultNow(),
+	updateAt: timestamp('update_at', { mode: 'date' }).defaultNow(),
+});
+
+export const profileRelation = relations(profileTable, ({ many }) => ({
+	comments: many(commentTable),
+}));
 
 export const commentTable = pgTable('comment', {
 	id: uuid('id').primaryKey().defaultRandom(),
+	profileId: uuid('profile_id')
+		.notNull()
+		.references(() => profileTable.id),
 	feedback: text().notNull(),
 	avatarUrl: varchar('avatar_url'),
 	pageId: varchar('page_id').notNull(),
@@ -10,3 +28,7 @@ export const commentTable = pgTable('comment', {
 	createdAt: timestamp('create_at', { mode: 'date' }).defaultNow(),
 	updateAt: timestamp('update_at', { mode: 'date' }).defaultNow(),
 });
+
+export const commentRelation = relations(commentTable, ({ one }) => ({
+	profile: one(profileTable, { fields: [commentTable.profileId], references: [profileTable.id] }),
+}));
--- lib/nextAuth/options.ts
+++ lib/nextAuth/options.ts
@@ -1,4 +1,6 @@
+import { db } from '@/lib/db';
 import { AuthOptions } from 'next-auth';
+import { profileTable } from '@/lib/db/schema';
 import GoogleProvider from 'next-auth/providers/google';
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET)
@@ -17,7 +19,26 @@ export const authOptions: AuthOptions = {
 	callbacks: {
 		async signIn({ user }) {
 			console.log({ user });
-			return true;
+			try {
+				if (!user.email || !user.id || !user.name || !user.email) return false;
+
+				const existingProfile = await db.query.profileTable.findFirst({
+					where: (table, { eq, or }) => or(eq(table.userId, user.id), eq(table.email, user.email!)),
+				});
+				if (existingProfile) return true;
+
+				await db.insert(profileTable).values({
+					userId: user.id,
+					name: user.name,
+					email: user.email,
+					imageUrl: user.image,
+				});
+
+				return true;
+			} catch (error) {
+				console.error('Failed to Sign In', { error });
+				return false;
+			}
 		},
 		async jwt({ token, user }) {
 			if (user) {
